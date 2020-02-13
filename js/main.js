a823847ac59998e860c60ff1970e58f41846b9d0
Your NameTamir <tamirn7@gmail.com>
2020-02-13 20:06:59 +0200

    my second commit

--- js/main.js
+++ js/main.js
@@ -1,10 +1,6 @@
 'use strict'
 var gCanvas
 var gCtx
-var gFont = 30
-var gX = 200
-var gYTop = 50
-var gYbottom = 350
 
 
 
@@ -12,30 +8,88 @@ var gYbottom = 350
 function onInit() {
     gCanvas = document.querySelector('#canvas');
     gCtx = gCanvas.getContext('2d');
-
+    renderCanvas()
 }
 
 
-function renderCanvas(text = 'Write something funny!') {
+
+function renderImgs() {
+    var strHTMLs = gImges.map(function(img) {
+        return `
+        <a> <img src="${img.imgUrl} " onclick="onOpenEditor(this) "> </a>`
+    })
+    var elImgesList = document.querySelector('.img-container');
+    elImgesList.innerHTML = strHTMLs.join('');
+}
+
+function renderCanvas() {
+    renderImgs()
     gCtx.textAlign = "center";
     var img = new Image()
     img.src = gMeme.img
+
     img.onload = () => {
         gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height)
-        writeOnImg(text)
+        gMeme.lines.forEach((memeObj, idx) => {
+            var font = memeObj.size
+            var color = memeObj.color
+            var txt = memeObj.txt
+            var pos = memeObj.linePosY
+
+            writeOnImg(txt, idx, pos, color, font)
+        });
     }
 }
 
 
-function writeOnImg(text) {
-    var currLineIdx = onChangingLine()
-    console.log(currLineIdx);
 
-    gCtx.font = gFont + 'px IMPACT'
-    gCtx.strokeStyle = 'white'
-    gCtx.fillText(text, gX, gYTop);
-    gCtx.strokeText(text, gX, gYTop)
+
+function writeOnImg(text, idx, pos, color, font) {
+
+    gCtx.font = font + 'px IMPACT'
+    gCtx.strokeStyle = 'black'
+    gCtx.fillStyle = color
+    gCtx.fillText(text, gX, pos);
+    gCtx.strokeText(text, gX, pos)
     gCtx.textAlign = "center";
-    // gMeme.lines[currLineIdx].txt = text
+    gMeme.lines[idx].txt = text
 
+}
+
+function onApplayColor(id) {
+    setColor(id)
+    renderCanvas()
+}
+
+function onChangingLine() {
+    toggleLine()
+    let text = document.getElementById('text')
+    text.value = ''
+}
+
+function onChangingLineHeight(id) {
+    changeLineHeight(id)
+    renderCanvas()
+}
+
+
+function onChangingFontSize(id) {
+    changeFontSize(id)
+    renderCanvas()
+}
+
+function onOpenEditor(el) {
+    openEditor(el)
+    renderCanvas()
+}
+
+
+function onAddLine() {
+    addLine()
+    renderCanvas()
+
+}
+
+function onCloseEditor() {
+    closeEditor()
 }
--- js/meme-service.js
+++ js/meme-service.js
@@ -1,85 +1,140 @@
 'use strict'
-var gNextId = 0
 var isWriting = false
+var gX = 250
+var gFont = 30
+var gNextId = 0
+var gImges = createImgs()
+
+function createImgs() {
+
+    var img = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18']
+        .map(createImg)
+
+    return img;
+}
+
+function createImg(key) {
+    return {
+        id: gNextId++,
+        key: key,
+        imgUrl: `meme-imgs (square)/${key}.jpg`
+    }
+}
+
 var gMeme = {
     img: 'http://127.0.0.1:5500/meme-imgs%20(square)/1.jpg',
-    // selectedImgId: 5,
+    // selectedImgId: 0,
     selectedLineIdx: 0,
 
     lines: [
-        { txt: 'I never eat Falafel', size: 20, align: 'left', color: 'red' },
-        { txt: 'Thats true, he didnt', size: 20, align: 'left', color: 'red' },
+        { txt: 'Write something funny!', size: 30, align: 'left', color: 'white', linePosY: 50 },
+        { txt: 'And also here, be funny.', size: 30, align: 'left', color: 'white', linePosY: 460 },
     ]
+
 }
 
 
 
-function writeLine() {
+function writeLine(val, idx) {
     if (!isWriting) {
         return
     } else {
-        let elText = document.getElementById('text')
-        let text = elText.value
-        renderCanvas(text)
+
+        gMeme.lines[idx].txt = val
+        renderCanvas(val, idx)
     }
+}
+
+
+
+function change(val) {
+    isWriting = true
+    var curIdx = gMeme.selectedLineIdx
+    writeLine(val, curIdx)
+}
+
+function changeFontSize(id) {
+    var currLineFont = gMeme.lines[gMeme.selectedLineIdx].size
+
+
+    if (id === 1) {
+        gCtx.font.split(' ');
+        var newSize = (currLineFont += 1) + 'px';
+        gCtx.font = newSize + ' ' + 'IMPACT'; /// using the last part
+    } else {
+        var cFont = gCtx.font;
+        gCtx.font.split(' ');
+        var newSize = (currLineFont -= 1) + 'px';
+        gCtx.font = newSize + ' ' + 'IMPACT';
+    }
+
+    gMeme.lines[gMeme.selectedLineIdx].size = currLineFont
+}
+
+
+
+
+
+function setColor(id) {
+    let fontColor = document.getElementById(id).value
+    gMeme.lines[gMeme.selectedLineIdx].color = fontColor
+
+}
+
+function downloadMeme(elLink) {
+    var imgContent = gCanvas.toDataURL();
+    elLink.href = imgContent
+    let text = document.getElementById('text')
+    text.value = ''
+}
+
+function toggleLine() {
+    var currLineIdx = gMeme.selectedLineIdx
+    if (currLineIdx === 0 || currLineIdx < gMeme.lines.length - 1) {
+        currLineIdx++
+
+    } else if (currLineIdx === gMeme.lines.length - 1) {
+        currLineIdx = 0
+
+    }
+
+    gMeme.selectedLineIdx = currLineIdx
+}
+
+function changeLineHeight(id) {
+    var currLinePos = gMeme.lines[gMeme.selectedLineIdx].linePosY
+
+    if (id === 1) {
+        currLinePos += 10
+    } else {
+        currLinePos -= 10
+    }
+
+    gMeme.lines[gMeme.selectedLineIdx].linePosY = currLinePos
 }
 
 function openEditor(el) {
     document.querySelector('.editor-container').classList.remove('hidden')
     document.querySelector('.container').classList.add('hidden')
     gMeme.img = el.src
-    renderCanvas()
 }
 
-function change(val) {
-    isWriting = true
-    writeLine(val)
-}
 
-function onChangingFontSize(id) {
-    if (id === 1) {
-        gCtx.font.split(' ');
-        var newSize = (gFont += 5) + 'px';
-        gCtx.font = newSize + ' ' + 'IMPACT'; /// using the last part
-    } else {
-        var cFont = gCtx.font;
-        gCtx.font.split(' ');
-        var newSize = (gFont -= 5) + 'px';
-        gCtx.font = newSize + ' ' + 'IMPACT';
+function addLine() {
+    let newLine = {
+        txt: 'You want more, ah?',
+        size: 30,
+        align: 'left',
+        color: 'white',
+        linePosY: 250
     }
-    change()
-    renderCanvas()
+    gMeme.lines.push(newLine)
+    gMeme.selectedLineIdx = 2
+    let text = document.getElementById('text')
+    text.value = ''
 }
 
-
-function onChangingLineHeight(id) {
-    if (id === 1) {
-        gYTop += 10
-    } else {
-        gYTop -= 10
-    }
-    change()
-}
-
-
-
-
-function onChangingLine() {
-
-    var currLineIdx = gMeme.selectedLineIdx
-    if (currLineIdx === 0) {
-        currLineIdx++
-    } else if (currLineIdx >= gMeme.lines.length - 1) {
-        currLineIdx = 0
-    }
-    gMeme.selectedLineIdx = currLineIdx
-    return currLineIdx
-}
-
-
-function saveMeme(elLink) {
-    var imgContent = gCanvas.toDataURL();
-    elLink.href = imgContent
-    let elUpperText = document.getElementById('text')
-    elUpperText.value = ''
+function closeEditor() {
+    document.querySelector('.editor-container').classList.add('hidden')
+    document.querySelector('.container').classList.remove('hidden')
 }
